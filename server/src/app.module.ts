227874ec298e7ede2c9bb5bd6ab8e63bb3cec862
Тюрин Данил <prowokatorkraft>
2026-03-12 01:28:06 +0400

    Created projects client, server

---
+++ server/src/app.module.ts
@@ -0,0 +1,16 @@
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'client', 'dist'), // путь к собранным файлам Vue
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
