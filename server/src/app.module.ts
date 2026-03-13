aab9069e37d62d67159b16ec0d0c375ef8ccbfaf
Тюрин Данил <prowokatorkraft>
2026-03-14 01:04:50 +0400

    add postgres to vercel

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: '.env.local',
       ignoreEnvFile: false,
     }),
     ServeStaticModule.forRoot({
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,13 +4,10 @@ import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   dotenv.config({
-    path: '.env',
+    path: '.env.local.local',
   });
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
-  console.info(process.env.POSTGRES_HOST);
-  console.info(process.env.POSTGRES_USER);
-  console.info(process.env.POSTGRES_PASSWORD);
 }
 bootstrap();
